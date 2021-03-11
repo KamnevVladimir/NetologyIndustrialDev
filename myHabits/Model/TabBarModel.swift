714f1af60914fec30a45de9902c53af23288e4cd
Kamnev Vladimir <trejlerfilms@gmail.com>
2021-03-11 23:40:47 +0300

    Delete comment in TabBarModel

--- myHabits/Model/TabBarModel.swift
+++ myHabits/Model/TabBarModel.swift
@@ -1,12 +1,6 @@
 import UIKit
 
 struct TabBarModel {
-//    let infoCircle: UIImage = {
-//        let image = #imageLiteral(resourceName: "info")
-//        let resizable = image.resizableImage(withCapInsets: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0), resizingMode: .stretch)
-//        return resizable
-//    }()
-    
     static let icons: [UIImage] = [#imageLiteral(resourceName: "habits_tab_icon"), #imageLiteral(resourceName: "info")]
     static let titles: [String] = ["Привычки", "Информация"]
 }
